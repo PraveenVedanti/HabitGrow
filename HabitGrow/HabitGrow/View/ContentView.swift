c3905b35361a0bf9fae752fc6abe61d89dfdb3fc
Praveen Vedanti <praveenvedanti11@gmail.com>
2025-06-25 23:03:39 -0700

    Add Content view

---
+++ HabitGrow/HabitGrow/View/ContentView.swift
@@ -0,0 +1,50 @@
+//
+//  ContentView.swift
+//  HabitGrow
+//
+//  Created by Praveen Kumar Vedanti on 6/25/25.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+    var body: some View {
+        
+        TabView {
+            Tab {
+                DashboardView()
+            } label: {
+                VStack {
+                    Image(systemName: "chart.bar")
+                    Text("dashboard")
+                }
+            }
+            
+            Tab {
+                HabitView()
+            } label: {
+                VStack {
+                    Image(systemName: "figure.run")
+                        .foregroundStyle(Color.orange)
+                    Text("habit")
+                }
+            }
+            
+            Tab {
+                SettingsView()
+            } label: {
+                VStack {
+                    Image(systemName: "gear")
+                    Text("settings")
+                }
+            }
+        }
+        .tint(Color.teal)
+    }
+}
+
+#Preview {
+    ContentView()
+        .modelContainer(for: Item.self, inMemory: true)
+}
