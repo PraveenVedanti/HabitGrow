1ac4033d278ee5edbe5a27a08c8b12bf4e2f2caf
Praveen Vedanti <praveenvedanti11@gmail.com>
2025-06-25 22:39:08 -0700

    Added new views for tab items

---
+++ HabitGrow/HabitGrow/View/DashboardView.swift
@@ -0,0 +1,16 @@
+//
+//  DashboardView.swift
+//  HabitGrow
+//
+//  Created by Praveen Kumar Vedanti on 6/25/25.
+//
+
+import SwiftUI
+import SwiftData
+
+
+struct DashboardView: View {
+    var body: some View {
+        Text("Dashboard view")
+    }
+}
---
+++ HabitGrow/HabitGrow/View/HabitView.swift
@@ -0,0 +1,15 @@
+//
+//  HabitView.swift
+//  HabitGrow
+//
+//  Created by Praveen Kumar Vedanti on 6/25/25.
+//
+
+import SwiftUI
+import SwiftData
+
+struct HabitView: View {
+    var body: some View {
+        Text("Habit view")
+    }
+}
